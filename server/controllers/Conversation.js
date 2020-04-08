7c25de738b18ed084d9a766022ba0d1d27e8e00c
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-04-08 13:29:21 -0400

    Updated sendMessage method. Added createConversation and getConversations methods

--- server/controllers/Conversation.js
+++ server/controllers/Conversation.js
@@ -8,9 +8,43 @@ const sendMessage = (req, res) => {
         return;
     }
     const newMessage = new models.Message.MessageModel({
-        text: req.body.message
+        text: req.body.message,
+        owner: req.session.account._id,
+        convo: req.session.convo._id
     });
     newMessage.save().then(() => {
-        
+
+    })
+};
+
+const createConversation = (req, res) => {
+    const convoData = {
+        title: req.body.title,
+        owner: req.session.account._id
+    }
+
+    const newConvo = new models.Conversation.ConversationModel(convoData);
+
+    const newConvoPromise = newConvo.save();
+
+    newConvoPromise.then(() => {
+        res.json({success: true});
+    })
+
+    newConvoPromise.catch((e) => {
+        res.status(400).json({error: 'An error occurred'});
     })
 }
+
+const getConversations = (req, res) => {
+    return Conversation.ConversationModel.findByOwner(req.session.account._id, (err, docs) => {
+        if (err) {
+            return res.status(400).json({error: 'An error occurred'});
+        }
+        return res.json({convos: docs});
+    })
+}
+
+module.exports.sendMessage = sendMessage;
+module.exports.createConversation = createConversation;
+module.exports.getConversations = getConversations;

a8907babce3ebfade984a9a04b05c2fac9b5e1f0
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-04-08 13:33:20 -0400

    Deleted Domo components and methods, replaced them with Conversation components and methods

--- client/app/app-client.js
+++ client/app/app-client.js
@@ -1,92 +1,77 @@
-const handleDomo = (e) => {
-    e.preventDefault();
-
-    if ($("#domoName").val() == '' || $("#domoAge").val() == '') {
-        handleError("RAWR! All fields are required");
-        return false;
-    }
-
-    sendAjax('POST', $("domoForm").attr("action"), $("#domoForm").serializeArray(), function() {
-        loadDomosFromServer();
-    });
-
-    return false;
-}
-
-const DomoForm = (props) => {
+const Conversation = (props) => {
     return (
-        <form id="domoForm"
-            onSubmit={handleDomo}
-            name="domoForm"
-            action="/maker"
-            method="POST"
-            className="domoForm"
-        >
-            <label htmlFor="name">Name: </label>
-            <input id="domoName" type="text" name="name" placeHolder="Domo Name"/>
-            <label htmlFor="age">Age: </label>
-            <input id="domoAge" type="text" name="age" placeHolder="Domo Age"/>
-            <input type="hidden" name="_csrf" value={props.csrf}/>
-            <input type="hidden" name="domoId" value={props._id}/>
-            <input className="makeDomoSubmit" type="submit" value="Make Domo"/>
-        </form>
-    )
-}
-
-const handleEditDomo = (e, domoId) => {
-    e.preventDefault();
-
-    if ($(`${"#editDomoForm_" + domoId} #domoName`).val() == '' || $(`${"#editDomoForm_" + domoId} #domoAge`).val() == '') {
-        handleError("RAWR! All fields are required");
-        return false;
-    }
-    let test = $(`${"#editDomoForm_" + domoId}`);
-    let testg = test.serializeArray();
-    sendAjax('POST', $(`${"#editDomoForm_" + domoId}`).attr("action"), $(`${"#editDomoForm_" + domoId}`).serializeArray(), loadDomosFromServer);
-
-    return false;
-}
-
-const EditDomoForm = (props) => {
-    return (
-        <form id={"editDomoForm_" + props.domoId}
-            // with help from https://stackoverflow.com/questions/44917513/passing-an-additional-parameter-with-an-onchange-event
-            onSubmit={(e) => handleEditDomo(e, props.domoId)}
-            name="editDomoForm"
-            action="/editDomo"
-            method="POST"
-            className="domoForm"
-        >
-            <label htmlFor="name">New Name: </label>
-            <input id="domoName" type="text" name="name" placeHolder="Domo Name"/>
-            <label htmlFor="age">New Age: </label>
-            <input id="domoAge" type="text" name="age" placeHolder="Domo Age"/>
-            <input type="hidden" name="_csrf" value={props.csrf}/>
-            <input type="hidden" name="_id" value={props.domoId}/>
-            <input className="makeDomoSubmit" type="submit" value="Edit Domo"/>
-        </form>
+        <h3>
+            {props.title}
+        </h3>
     )
 }
 
 
-class DomoList extends React.Component {
+class NewConversation extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isBeingEdited: false};
+        this.state = {canShow: false, csrf: props.csrf};
+        this.show = this.show.bind(this);
+        this.hide = this.hide.bind(this);
+        this.handleNewConversation = this.handleNewConversation.bind(this);
+        
+    }
+    
+    show() {
+        this.setState({canShow: true});
     }
 
+    hide() {
+        this.setState({canShow: false});
+    }
 
+    handleNewConversation (e){
+        e.preventDefault();
+        if ($("#convoTitle").val() == '') {
+            alert("Must enter a title");
+            return;
+        }
+        let test = $("#newConvoForm").serializeArray();
+        sendAjax('POST', '/createConversation', test, loadConversations)
+    }
 
     render() {
-        if (this.props.domos.length === 0) {
+        return (
+            <>
+                <Button variant = "primary" onClick={this.show}>+</Button>
+                <Modal show={this.state.canShow} onHide={this.hide}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>New Conversation</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form id="newConvoForm" onSubmit={this.handleNewConversation}>
+                            <Form.Group controlId="convoTitle">
+                                <Form.Label>Title: </Form.Label>
+                                <Form.Control type="text" name="title"></Form.Control>
+                                <Form.Control type="hidden" name="_csrf" value={this.state.csrf} />
+                                <Button variant="primary" type="submit">
+                                    Create
+                                </Button>
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
+            </>
+        )
+    }
+}
+
+const ConversationList = (props) => {
+        if (props.convos.length === 0) {
             return (
-                <div className="domoList">
-                    <h3 className="emptyDomo">No Domos yet</h3>
+                <div className="convoList">
+                    <h3 className="emptyList">No Conversations yet</h3>
+                    <NewConversation csrf={props.csrf}/>
                 </div>
             );
         }
     
-        const domoNodes = this.props.domos.map(function(domo) {
+        const convoNodes = props.convos.map(function(c) {
             return (
                 // <div key={domo._id} className="domo">
                 //     <img src = "/assets/img/domoface.jpeg" alt = "Domo face" className="domoFace" />
@@ -94,67 +79,30 @@ class DomoList extends React.Component {
                 //     <h3 className="domoAge">Age: {domo.age}</h3>
                 //     <button onClick = {this.toggleIsBeingEdited}>Edit</button>
                 // </div>
-                <DomoItem _id={domo._id} name={domo.name} age={domo.age}/>
+                <Conversation title = {c.title}/>
             );
         }, this);
 
     
         return (
-            <div className="domoList">
-                {domoNodes}
+            <div className="convoList">
+                {convoNodes}
+                <NewConversation csrf={props.csrf}/>
             </div>
         )
-    }
 };
 
-class DomoItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isBeingEdited: false};
-        this.toggleIsBeingEdited = this.toggleIsBeingEdited.bind(this);
-    }
-
-    toggleIsBeingEdited() {
-        this.setState((state, props) =>( {
-            isBeingEdited: !state.isBeingEdited
-        }));
-    }
-
-
-    render() {
-        return(
-        <div>
-            <div key={this.props._id} className="domo">
-                <img src = "/assets/img/domoface.jpeg" alt = "Domo face" className="domoFace" />
-                <h3 className="domoName">Name: {this.props.name}</h3>
-                <h3 className="domoAge">Age: {this.props.age}</h3>
-                <button onClick = {this.toggleIsBeingEdited}>Edit</button>
-            </div>
-            {this.state.isBeingEdited ? <EditDomoForm csrf={DomoItem.csrf || ''} domoId = {this.props._id}/> : ''}
-        </div>
-        )
-    }
-}
-
-const loadDomosFromServer = () => {
-    sendAjax('GET', '/getDomos', null, (data) => {
+const loadConversations = (csrf) => {
+    sendAjax('GET', '/getConversations', null, (data) => {
         ReactDOM.render(
-            <DomoList domos = {data.domos} />, document.querySelector("#domos")
+            <ConversationList convos={data.convos} csrf={csrf}/>, document.querySelector("#convos")
         );
     });
 };
 
 const setup = function(csrf) {
-    DomoItem.csrf = csrf;
-    ReactDOM.render(
-        <DomoForm csrf={csrf} />, document.querySelector("#makeDomo")
-    );
 
-    ReactDOM.render(
-        <DomoList domos={[]} />, document.querySelector("#domos")
-    );
-
-    loadDomosFromServer();
+    loadConversations(csrf);
 };
 
 const getToken = () => {

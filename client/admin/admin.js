1b24810dfe1f98982dd0c70c50a7aed296ef9942
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-04-16 18:56:21 -0400

    Created admin.js. Added scripts to build its bundle

---
+++ client/admin/admin.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    getToken();
+});
+
+const getToken = () => {
+    sendAjax('GET', '/getToken', null, (result) => {
+        setup(result.csrfToken);
+    });
+};
+
+const setup = function(csrf) {
+    loadUsers(csrf);
+};
+
+const User = (props) => {
+    return (
+        <>
+            <h1>{props.name}'s Conversations</h1>
+            <ConversationList convos={props.conversations} />
+        </>
+    )
+}
+
+const UserList = (props) =>  {
+    let users = props.userData.map(u => {
+        return <User name={u.name} conversations={u.conversations}/>
+    });
+    return (
+        <>
+            {users}
+        </>
+    )
+}
+
+const loadUsers = (csrf) => {
+    sendAjax('GET', '/getUsers', null, (data) => {
+        ReactDOM.render(
+            <UserList userData={data} csrf={csrf}/>, document.querySelector("#users")
+        );
+    });
+
+}

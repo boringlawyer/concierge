d4fe75ab56e8f30ae7e2b5802eec74ac0a528191
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-04-17 22:45:59 -0400

    The other half of the "server shutdown fix"

--- client/chat/chat.js
+++ client/chat/chat.js
@@ -1,11 +1,6 @@
 const conversationId = location.href.split('/')[4]
 
-const socket = io({
-    query: 
-    {
-        conversationId
-    }
-});
+const socket = io();
 
 socket.on('loadMsgs', (messages) => {
     for (let m of messages) {
@@ -21,3 +16,9 @@ $('#sendMsg').on('click', () => {
     socket.emit('message', $('#msgText').val());
     // $('#messages').append($('<li>').text($('#msgText').val()));
 });
+
+// if the server is down, and then is restarted when the chat page is open, it causes an error.
+// This fix ensures that when the error is caught on the server, it tells the page to refresh
+socket.on('refresh', () => {
+    window.location.assign(window.location.href);
+})
